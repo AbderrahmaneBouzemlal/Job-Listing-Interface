28cfe274c8230ae5b68c43d586db259d1e1ec8e9
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2025-09-25 11:37:43 +0800

    configure the link

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://AbderrahmaneBouzemlal/Job-Listing-Interface',
   plugins: [svelte(), tailwindcss()],
 })
